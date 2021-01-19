5d8c0620c7037e5b82186be48db3f7fb760fe3f2
Prinalchandra.Patel <Scricket@10>
2021-01-19 12:48:21 -0700

    added the code to start the h2 database in server mode.

--- src/main/java/com/coding/hstest/HSTestApplication.java
+++ src/main/java/com/coding/hstest/HSTestApplication.java
@@ -4,8 +4,12 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.h2.tools.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.sql.SQLException;
 
 @SpringBootApplication
 @OpenAPIDefinition(info = @Info(title = "Person API", version = "v1"))
@@ -14,6 +18,12 @@ class HSTestApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HSTestApplication.class, args);
+    }
+
+    // Second App
+    @Bean(initMethod = "start", destroyMethod = "stop")
+    public Server h2Server() throws SQLException {
+        return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "8091");
     }
 
 }
